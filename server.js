0321ecc2ad80f23e7740a87757c1d8afa233e28a
Valery Semenenko <gearmobile@gmail.com>
2018-11-04 22:17:06 +0400

    add server

---
+++ server.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require('apollo-server');
+const TODOS = [
+  { task: 'Buy milk', completed: false },
+  { task: 'Bring car', completed: true }
+];
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+`;
+const server = new ApolloServer({
+  typeDefs
+});
+server.listen().then(({ url }) => console.log(`Start Apollo Server on ${url}`));
